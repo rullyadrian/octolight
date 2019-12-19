2c2aa7a9678fc6c664c106c2e4ad3927365f3528
Rully Adrian Santosa <rully@santosa.id>
2019-12-19 22:37:14 +0800

    Update sketch sequential with glow

--- sketches/sequential/sequential.ino
+++ sketches/sequential/sequential.ino
@@ -1,19 +1,63 @@
-void setup() {
-  for (int i = 0; i <= 8; i++) {
-    pinMode(i, OUTPUT); 
+#define NUM_LIGHTS 8
+#define PERIOD 100
+#define HALF_PERIOD (PERIOD / 2)
+#define OVERLAP 20
+#define REST 100
+#define MAX_VALUE 255
+#define DELAY 10
+
+int lights[] = {7, 6, 3, 2, 5, 4, 1, 0};
+int values[] = {0, 0, 0, 0, 0, 0, 0, 0};
+int t = 0;
+
+void setup()
+{
+  for (int i = 0; i <= 8; i++)
+  {
+    pinMode(i, OUTPUT);
   }
 }
 
-int lights[] = {7, 6, 3, 2, 5, 4, 1, 0};
-int active = 0;
+void loop()
+{
+  calculateValues();
+  writeValues();
 
-void loop() {
-  for (int i = 0; i <= 8; i++) {
-    analogWrite(i, 0);
+  t = (t + 1) % (NUM_LIGHTS * (PERIOD - OVERLAP) + OVERLAP + REST);
+  delay(DELAY);
+}
+
+void calculateValues()
+{
+  for (int i = 0; i < NUM_LIGHTS; i++)
+  {
+    values[i] = calculateValue(i, t - i * (PERIOD - OVERLAP));
   }
-  analogWrite(lights[active], 255);
-  
-  active = ( active + 1 ) % 8;
-  
-  delay(1000);
+}
+
+int calculateValue(int idx, int t)
+{
+  float value = 0.0;
+
+  if (t >= 0)
+  {
+    if (t < HALF_PERIOD)
+    {
+      value = float(t) / HALF_PERIOD * MAX_VALUE;
+    }
+    else if (t < PERIOD)
+    {
+      value = (1 - (float(t) - HALF_PERIOD) / HALF_PERIOD) * MAX_VALUE;
+    }
+  }
+
+  return value;
+}
+
+void writeValues()
+{
+  for (int i = 0; i < NUM_LIGHTS; i++)
+  {
+    analogWrite(lights[i], values[i]);
+  }
 }
